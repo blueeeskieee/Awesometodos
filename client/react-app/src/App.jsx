c830d4e344661fae320d93a8f71e6437feb8dfea
blueeeskieee <secusanab@gmail.com>
2026-04-05 18:18:02 +0800

    Another update for my code

--- client/react-app/src/App.jsx
+++ client/react-app/src/App.jsx
@@ -1,27 +1,3 @@
-/*import { useEffect, useState } from "react";
-
-export default function App() {
-  const [message, setMessage] = useState("");
-  
-  useEffect(() => {
-    const getTodos = async () => {
-      const res = await fetch("/api/todos");
-      const todos = await res.json();
-
-      setMessage(todos.mssg);
-    };
-
-    getTodos();
-  }, [])
-
-  return (
-    <main className="container">
-      <h1>Awesome Todos</h1>
-      {message && <p>{message}</p>}
-    </main>
-  );
-}
-*/
 import { useEffect, useState } from "react";
 import Todo from "./todo";
 export default function App() {
--- server/models/index.js
+++ server/models/index.js
@@ -1,29 +1,3 @@
-/*
-const { MongoClient } = require("mongodb")
-
-const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri)
-
-let database
-
-async function connectDB() {
-    await client.connect()
-    database = client.db("myDatabase") // change if needed
-    console.log("MongoDB Connected")
-}
-
-function getCollection(name) {
-    if (!database) {
-        throw new Error("Database not initialized")
-    }
-    return database.collection(name)
-}
-
-module.exports = {
-    connectDB,
-    getCollection
-}
-*/
 const { MongoClient } = require("mongodb");
 // Add this line to load the .env file
 require("dotenv").config();
